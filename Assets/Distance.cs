2799010b23ca2ed07bada771fcdb3569d3639a30
Filip <f.cieminski@gmail.com>
2019-11-03 13:24:28 +0100

    earth and sun placed and scaled, earth script almost ready

---
+++ Assets/Distance.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Distance : MonoBehaviour
+{
+    public GameObject earth;
+    public GameObject sun;
+    public float Distance_;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Distance_ = Vector3.Distance(earth.transform.position, sun.transform.position);
+    }
+}
---
+++ Assets/Orbit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float time;
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform target = transform.parent;
+
+        if (target != null)
+        {
+            transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
+        }
+    }
+}
