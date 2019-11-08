06be2f761aeebe8e6eb674b8c1cbce63297330e8
Filip <f.cieminski@gmail.com>
2019-11-08 22:58:49 +0100

    animation on menu works

--- Assets/MenuSwitch.cs
+++ Assets/MenuSwitch.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class MenuSwitch : MonoBehaviour
 {
     public Transform panel;
+    private Vector3 newPos;
+    float value = 0;
+    float speed = 10;
 
+    void Start()
+    {
+        newPos = new Vector3(panel.position.x, panel.position.y, panel.position.z);
+    }
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * speed);
+    }
     public void switchMenu()
     {
-        float value = 0;
         value = panel.position.x < 0 ? Mathf.Abs(panel.position.x) : -panel.position.x;
-
-        transform.position = new Vector3(value, panel.position.y, panel.position.z);
-
+        newPos = new Vector3(value, panel.position.y, panel.position.z);
     }
 }
