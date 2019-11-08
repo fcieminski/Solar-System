b70146204541d193a2075c560b411fd019b03e2c
Filip <f.cieminski@gmail.com>
2019-11-08 20:59:48 +0100

    camera switch on button

---
+++ Assets/ChoosePlanet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoosePlanet : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject planet;
+    public void GoToPlanet()
+    {
+        if (Camera.main)
+        {
+            Camera.main.enabled = false;
+        }
+        // current = Camera.current;
+        camera.enabled = true;
+    }
+}
