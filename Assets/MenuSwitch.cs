57610df653933f7a8c9e55b3aac559c05596a952
Filip <f.cieminski@gmail.com>
2019-11-08 21:50:30 +0100

    menu switch works

---
+++ Assets/MenuSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSwitch : MonoBehaviour
+{
+    public Transform panel;
+
+    public void switchMenu()
+    {
+        float value = 0;
+        value = panel.position.x < 0 ? Mathf.Abs(panel.position.x) : -panel.position.x;
+
+        transform.position = new Vector3(value, panel.position.y, panel.position.z);
+
+    }
+}
